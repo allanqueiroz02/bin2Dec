9d974de33304136a103dbaafabe95f6f03aa1a69
Allan Dawid <allan.queiroz.dev@gmail.com>
2024-10-25 00:12:53 -0300

    implementada lógica de conversão

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log("Script carregado!");
+const binaryInput = document.getElementById("binary-value");
+const resultSpan = document.getElementById("resultado");
+const convertButton = document.getElementById("convert-button");
+
+convertButton.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (binaryInput) {
+    const formattedValue = [...binaryInput.value];
+
+    const convertedValue = formattedValue.reduce((acc, item, index) => {
+      const castedValue = Number(item);
+      const exponent = formattedValue.length - (index + 1);
+      const result = castedValue * 2 ** exponent;
+
+      return acc + result;
+    }, 0);
+
+    resultSpan.innerText = convertedValue;
+  }
+});
