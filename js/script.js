5763847deaf61af7c30de05ee60fb6e07d0d80dd
Allan Dawid <allan.queiroz.dev@gmail.com>
2024-10-26 16:31:44 -0300

    criado warning alert

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 const binaryInput = document.getElementById("binary-value");
 const resultSpan = document.getElementById("resultado");
 const convertButton = document.getElementById("convert-button");
+const warningContainer = document.querySelector(".container-warning");
+const closingWarningButton = document.querySelector(".closing-btn");
+
+function changeWarningState(state) {
+  if (state === "none") warningContainer.style.display = "none";
+  else warningContainer.style.display = "flex";
+}
 
 convertButton.addEventListener("click", (event) => {
   event.preventDefault();
@@ -18,14 +25,26 @@ convertButton.addEventListener("click", (event) => {
 
     resultSpan.innerText = convertedValue;
   }
+
+  changeWarningState("none");
 });
 
 binaryInput.addEventListener("keydown", (event) => {
-  console.log(">key", event.key);
-
   if (event.key === "Enter") console.log("Clicou no enter");
-  if (event.key !== "Enter" && event.key !== "0" && event.key !== "1"){
-    event.preventDefault()
-    alert('Não é permitido informar algo diferente de 0 ou 1.')
+  if (
+    event.key !== "Enter" &&
+    event.key !== "Backspace" &&
+    event.key !== "0" &&
+    event.key !== "1"
+  ) {
+    event.preventDefault();
+    changeWarningState("flex");
   }
+  if (event.key === "0" || event.key === "1") {
+    changeWarningState("none");
+  }
+});
+
+closingWarningButton.addEventListener("click", () => {
+  changeWarningState("none");
 });
